083f7c7075d94623c348f15da8f5f583613138cf
Shafah0717 <shafahmuhammed@gmail.com>
2025-04-28 23:18:15 +0530

    write main function routing

---
+++ main.go
@@ -0,0 +1,26 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+
+)
+
+
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/movies",getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies",createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("movies/{id}",deleteMovie).Methods("DELETE")
+
+	
+	fmt.Printf("starting server at port 8000")
+	log.Fatal(http.ListenAndServe("8000:",r))
+}
